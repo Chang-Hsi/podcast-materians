d2684e8f01bc0c8d19dd71838b558998c08927ad
Chang-Hsi <watasiwa8531@gmail.com>
2025-11-17 15:51:49 +0800

    build for github pages

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,17 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
-// 重點：base 一定要和 repo 名稱一致
 export default defineConfig({
+  // 一定要和 repo 名稱一致
   base: '/podcast-materians/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  // 這裡是重點：build 到 docs 資料夾
+  build: {
+    outDir: 'docs'
   }
 })
